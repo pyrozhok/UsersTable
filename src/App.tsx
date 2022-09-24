74e08e52d50b6597bd43b43b7d76f5863a34d7a1
Alexander Sopin <alex.sopin0803@gmail.com>
2022-09-24 14:32:30 +0700

    Add Users page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from "react";
-
+import Login from "./pages/Login/Login";
+import Users from "./pages/Users/Users";
 function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      <Users />
+    </div>
+  );
 }
 
 export default App;
