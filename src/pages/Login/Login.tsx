e41d24303b81280977d7c3cfcc75eb15c4798aa2
Alexander Sopin <alex.sopin0803@gmail.com>
2022-09-24 15:34:17 +0700

    Wrap component to div, add title

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -26,51 +26,54 @@ const Login = () => {
   const onSubmit: SubmitHandler<Inputs> = data => console.log(data);
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <Container component="main">
-        <TextField
-          defaultValue=""
-          {...register("email", {
-            required: true,
-            maxLength: 320,
-            pattern: emailPattern,
-          })}
-          margin="normal"
-          required
-          fullWidth
-          id="email"
-          label="Email Address"
-          name="email"
-          autoComplete="email"
-          autoFocus
-        />
-        <TextField
-          defaultValue=""
-          {...register("password", { required: true, minLength: 6 })}
-          margin="normal"
-          required
-          fullWidth
-          name="password"
-          label="Password (min 6 symbols)"
-          type="password"
-          id="password"
-          autoComplete="current-password"
-        />
+    <div className="container">
+      <div className="title">Log in</div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Container component="main">
+          <TextField
+            defaultValue=""
+            {...register("email", {
+              required: true,
+              maxLength: 320,
+              pattern: emailPattern,
+            })}
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+          />
+          <TextField
+            defaultValue=""
+            {...register("password", { required: true, minLength: 6 })}
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password (min 6 symbols)"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
 
-        <FormControlLabel
-          control={<Checkbox value="remember" color="primary" />}
-          label="Remember me"
-        />
-        <Button
-          onClick={handleSubmit(onSubmit)}
-          fullWidth
-          variant="contained"
-          sx={{ mt: 3, mb: 2 }}
-        >
-          Sign In
-        </Button>
-      </Container>
-    </form>
+          <FormControlLabel
+            control={<Checkbox value="remember" color="primary" />}
+            label="Remember me"
+          />
+          <Button
+            onClick={handleSubmit(onSubmit)}
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Sign In
+          </Button>
+        </Container>
+      </form>
+    </div>
   );
 };
 export default Login;
