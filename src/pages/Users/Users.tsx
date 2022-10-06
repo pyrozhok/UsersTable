d5485a9880a7cd6c3f64095f9989333c41039fa1
Alexander Sopin <alex.sopin0803@gmail.com>
2022-10-06 21:33:08 +0700

    Remove unused code

--- src/pages/Users/Users.tsx
+++ src/pages/Users/Users.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Navigate } from "react-router-dom";
+
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import CircularProgress from "@mui/material/CircularProgress";
